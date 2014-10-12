7cda444e9272bea1847f52d42b227c2646f8d862
sdonamayor <sdonamayor@users.noreply.github.com>
2014-10-12 12:02:43 +0200

    Create plot2.R

---
+++ plot2.R
@@ -0,0 +1,6 @@
+source("load_data.R")
+
+table <- prepareData()
+png(filename = "plot2.png", width = 480, height = 480, units = "px")
+plot(table$DateTime, table$Global_active_power, type = "l", xlab = "", ylab = "Global Active Power (kilowatts)")
+dev.off()

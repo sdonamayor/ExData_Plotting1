91173893e3f4cc37dfc8b158680d1fcec28a61e7
sdonamayor <sdonamayor@users.noreply.github.com>
2014-10-12 12:04:52 +0200

    Update plot1.R

--- plot1.R
+++ plot1.R
@@ -3,4 +3,5 @@ source("load_data.R")
 table <- prepareData()
 png(filename = "plot1.png", width = 480, height = 480, units = "px")
 hist(table$Global_active_power, main = "Global Active Power", xlab = "Global Active Power (kilowatts)", col = "red")
+
 dev.off()

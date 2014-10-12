270b501a9ed31b1c2efea14ebbc5721d31b15e24
sdonamayor <sdonamayor@users.noreply.github.com>
2014-10-12 12:15:54 +0200

    Update plot2.R

--- plot2.R
+++ plot2.R
@@ -1,7 +1,7 @@
 source("load_data.R")
 
-table <- prepareData()
+mydata <- prepareData()
 png(filename = "plot2.png", width = 480, height = 480, units = "px")
-plot(table$DateTime, table$Global_active_power, type = "l", xlab = "", ylab = "Global Active Power (kilowatts)")
+plot(mydata$DateTime, mydata$Global_active_power, type = "l", xlab = "", ylab = "Global Active Power (kilowatts)")
 
 dev.off()

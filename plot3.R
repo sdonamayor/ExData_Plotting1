8042afe03e5ee1e7f581d07d45c7aeb4ca2ce323
sdonamayor <sdonamayor@users.noreply.github.com>
2014-10-12 12:03:24 +0200

    Create plot3.R

---
+++ plot3.R
@@ -0,0 +1,10 @@
+source("load_data.R")
+
+table <- prepareData()
+png(filename = "plot3.png", width = 480, height = 480, units = "px")
+cols = c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3")
+plot(table$DateTime, table$Sub_metering_1, type = "l", xlab = "", ylab = "Energy sub metering")
+lines(table$DateTime, table$Sub_metering_2, type = "l", col = "red")
+lines(table$DateTime, table$Sub_metering_3, type = "l", col = "blue")
+legend("topright", lty = 1, lwd = 1, col = c("black", "blue", "red"), legend = cols)
+dev.off()
